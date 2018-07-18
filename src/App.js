eefa4751b3219a2030135c6745615aad1b9d7561
Jane Asawa <asawajane@gmail.com>
2018-07-18 14:16:00 -0700

    replaced src with new to-be-modified src

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import landmarks from "./landmarks.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    landmarks
+  };
+
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
+  };
+
+  // Map over this.state.landmarks and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Title>Landmarks List</Title>
+        {this.state.landmarks.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={landmark.id}
+            key={landmark.id}
+            name={landmark.name}
+            image={landmark.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
